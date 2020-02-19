f612f2711c43ebd868e06d6a69973b4cf8dd639a
Piotr Damer <piotrd@czuby.net>
2020-02-19 13:15:41 +0000

    Finalized the two-model (restaurant + reviews) app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+
+25.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone, category: category)
+  restaurant.save
+  puts "Saved restaurant ##{restaurant.id}"
+end

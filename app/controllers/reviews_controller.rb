7ae5c4d424ec14e2a826e6cdabcc23ac451f48c1
Piotr Damer <piotrd@czuby.net>
2020-02-19 13:48:32 +0000

    Refactored the code (2)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "restaurants/show"
     end
   end
 
